6b1520d13631acde5c0cba048d89ef2f9e96edfc
Ritika Saxena <ritikasaxena911@gmail.com>
2023-09-21 19:43:34 +0530

    created link shortner

--- server.js
+++ server.js
@@ -1,6 +1,27 @@
 const express=require('express');
+const shortId = require('shortid')
 const port=5000;
 const app=express();
+
+const url={};
+app.get('/shortner',(req,res)=>{
+    const u=req.query.u;
+    const id=shortId.generate();
+    url[id]=u;
+    res.send(`http://localhost:${port}/${id}`);
+})
+
+app.get('/:id',(req,res)=>{
+    const id=req.params.id;
+    const u=url[id];
+    if(u){
+        res.redirect(u);
+    }
+    else{
+        res.sendStatus(404)
+    }
+})
+
 app.listen(port,()=>{
     console.log(`Server starting on... ${port}`)
 })
