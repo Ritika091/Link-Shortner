50b1854cf497f140f6e153863c798d6ba80274f5
Ritika Saxena <ritikasaxena911@gmail.com>
2023-09-27 23:20:47 +0530

    made the site responsive

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,18 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
 import {Scissors} from 'lucide-react'
 export default function Home() {
+  const [link,setLink]=useState();
+  const [shorturl,setShorturl]=useState("");
+  const short=()=>{
+   fetch(`http://localhost:5000/${id}`,{
+    // method:"GET",
+    // body:JSON.stringify({
+    //   id:id
+    // })
+   }).then(res=>res.json())
+   setLink(res.json().result.full_short_link)
+  //  .then((result)=>{
+  //   console.log(result);
+  //  })
+   .catch(err=>console.log(err))
+  }
   return (
     <div className='Home'>
         <h1>Link Shortener</h1>
         <p>Shorten the long links here!</p>
         <div className="Search">
-            <input type="text" />
+            <input type="text" value={link} onChange={(e)=>{setLink(e.target.value)}}/>
+            <button onClick={()=>{short(link)}}>
             <Scissors className='icon' />
+            </button>
         </div>
         <div className="Result">
             <h2>Shorten Link- </h2>
-            <h2>hhtptpppp</h2>
+            <h2>{shorturl}</h2>
         </div>
     </div>
   )
